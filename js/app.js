c0133326191b9ddf8b8cf09a0f42daa1f763e385
Abi Vickery <vickeryabi@gmail.com>
2019-12-02 11:16:28 -0600

    not defined marvel and dc part 1

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var foo = baz;
+var foo = 'marvel'; // marvel is not defined
 {
     var iron = 'Iron Man';
     let tony = 'Tony Stark';
@@ -7,5 +7,17 @@ var foo = baz;
     console.log(tony);
     console.log(black);
 }
-console.log(baz);
-console.log(tony);
+// console.log(marvel); // not defined
+// console.log(tony); // tony is not defined
+
+var baz = 'dc';
+{
+    var bat = 'Batman';
+    let sup = 'Superman';
+    const lan = 'Green Lantern';
+    console.log(bat);
+    console.log(sup);
+    console.log(lan);
+}
+console.log(dc);
+console.log(bat);
