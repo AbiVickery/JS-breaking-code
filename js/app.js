4190cf835ce9ef782f7c75ed482d1a0974461783
Abi Vickery <vickeryabi@gmail.com>
2019-12-02 11:08:13 -0600

    vars and blocks

---
+++ js/app.js
@@ -0,0 +1,11 @@
+var foo = baz;
+{
+    var iron = 'Iron Man';
+    let tony = 'Tony Stark';
+    const black = 'Black Widow';
+    console.log(iron);
+    console.log(tony);
+    console.log(black);
+}
+console.log(baz);
+console.log(tony);
